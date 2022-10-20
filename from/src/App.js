cfb6b0897b45091855a53becc9168d4b9ceb8413
creatimarcas <110987206+avasscoled@users.noreply.github.com>
2022-10-20 04:41:53 -0500

    Actualizacion 20 octubre2022

---
+++ from/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Tienda de Ropa Avass Coled</h1>
+        <img src="./images/logo192.png" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
