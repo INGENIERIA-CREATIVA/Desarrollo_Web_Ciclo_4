d88c627c561fc43e4bd6281a6a391bd2d96fb37d
creatimarcas <110987206+avasscoled@users.noreply.github.com>
2022-10-20 07:23:11 -0500

    Segunda Actualizacion 20oct2022

--- from/src/App.js
+++ from/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import React from 'react';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
-        <h1>Tienda de Ropa Avass Coled</h1>
-        <img src="./images/logo192.png" alt="Aqui va el logo"></img>
+        <Header/>
+        <center>Contenido Aqui</center>
+        <center>Contenido Aqui</center>
+        <center>Contenido Aqui</center>
+        <center>Contenido Aqui</center>
+        <center>Contenido Aqui</center>
+        <center>Contenido Aqui</center>
+        <center>Contenido Aqui</center>
+        <center>Contenido Aqui</center>
+        <center>Contenido Aqui</center>
+        <Footer/>
     </div>
   );
 }
