f33d4f4d129b2c9107cfcaaf625ec3b28d0aeb7e
creatimarcas <110987206+avasscoled@users.noreply.github.com>
2022-11-02 20:30:48 -0500

    Actualizacion 02 noviembre 2022

--- back/app.js
+++ back/app.js
@@ -1,13 +1,19 @@
-const express =require("express");
-const app =express();
+const express=require("express");
+const app = express();
+const errorMiddleware= require("./middleware/errors")
 
 app.use(express.json());
 
 //Importar rutas
 const productos=require("./routes/products")
 
+
+
 app.use('/api',productos) //Sujeto a decision (ruta del navegador)
 
+
+//MiddleWares para manejar errores
+app.use(errorMiddleware)
+
 module.exports=app
-
-
+  
